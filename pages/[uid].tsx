a38a7cf397f9d73b24d44942431a591e4fdc4c18
suraj kumar <kumar.suraj@rwit.io>
2024-04-26 18:04:29 +0530

    intial commit

--- pages/[uid].tsx
+++ pages/[uid].tsx
@@ -1,36 +1,53 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 import Head from "next/head";
-import { isFilled } from "@prismicio/client";
+import { isFilled, asLink } from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
 
 import { components } from "@/slices";
 import { createClient } from "@/prismicio";
+
+type Params = { uid: string };
 
 export default function Page({
   page,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
-      {/* <Head>
+      <Head>
         <title>{page.data.meta_title}</title>
         {isFilled.keyText(page.data.meta_description) ? (
           <meta name="description" content={page.data.meta_description} />
         ) : null}
-      </Head> */}
+      </Head>
       <SliceZone slices={page.data.slices} components={components} />
     </>
   );
 }
 
-export async function getStaticProps({ previewData }: GetStaticPropsContext) {
+export async function getStaticProps({
+  params,
+  previewData,
+}: GetStaticPropsContext<Params>) {
   // The `previewData` parameter allows your app to preview
   // drafts from the Page Builder.
   const client = createClient({ previewData });
 
-  // The query fetches the page's data based on the current URL.
-  const page = await client.getSingle("home");
+  const page = await client.getByUID("page", params!.uid);
 
   return {
     props: { page },
+  };
+}
+
+export async function getStaticPaths() {
+  const client = createClient();
+
+  const pages = await client.getAllByType("page"); 
+
+  return {
+    paths: pages.map((page) => {
+      return asLink(page);
+    }),
+    fallback: false,
   };
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <script async defer src="https://static.cdn.prismic.io/prismic.js?new=true&repo=my-sites" />
       <body>
         <Main />
         <NextScript />

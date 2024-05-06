92cc3af0f301fbc2659ac1f528ed1b55ceb9044d
suraj kumar <kumar.suraj@rwit.io>
2024-05-06 18:13:31 +0530

    add locles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,49 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import { PrismicNextLink } from '@prismicio/next'
+
 import Head from "next/head";
 import { isFilled } from "@prismicio/client";
 import { SliceZone } from "@prismicio/react";
 
 import { components } from "@/slices";
 import { createClient } from "@/prismicio";
+import { getLocales } from "./lib/getLocales"
 
 export default function Page({
   page,
+  locales
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
-      {/* <Head>
+      <Head>
         <title>{page.data.meta_title}</title>
         {isFilled.keyText(page.data.meta_description) ? (
           <meta name="description" content={page.data.meta_description} />
         ) : null}
-      </Head> */}
+      </Head>
+      {/* <ul>
+        {locales.map((locale) => (
+          <li key={locale.id}>
+            <PrismicNextLink href={locale.url}>{locale.lang_name}</PrismicNextLink>
+          </li>
+        ))}
+      </ul> */}
       <SliceZone slices={page.data.slices} components={components} />
     </>
   );
 }
 
-export async function getStaticProps({ previewData }: GetStaticPropsContext) {
+export async function getStaticProps({ previewData, locale }: GetStaticPropsContext) {
   // The `previewData` parameter allows your app to preview
   // drafts from the Page Builder.
   const client = createClient({ previewData });
-
+  
   // The query fetches the page's data based on the current URL.
-  const page = await client.getSingle("home");
+  const page = await client.getSingle("home", { lang: locale });  
+  const locales = await getLocales(page, client)
 
-  return {
-    props: { page },
+  return {  
+    props: { page , locales},
+    revalidate: 60,
   };
 }
